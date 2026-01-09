d154e6b2bca5a856dcb9e6346af8fffcd59a943d
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-01-09 17:13:20 +0530

    Configured redux store with user slice

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import Body from "./component/Body.jsx";
 import Feed from "./component/Feed.jsx";
 import Login from "./component/Login.jsx";
 import Profile from "./component/Profile.jsx";
+import { Provider } from "react-redux";
+import appStore from "./store/appStore.js";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/" element={<Feed />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 };
