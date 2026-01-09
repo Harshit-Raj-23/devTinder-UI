013856c94b9f4bcc026e2c8b0c13169fc21da46e
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-01-09 22:34:46 +0530

    Handling Logout and restricted access to Logged In User

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,8 +1,34 @@
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants.js";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../store/userSlice.js";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      if (user) return;
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res?.data?.data));
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <div>
       <Navbar />
