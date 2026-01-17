b60e5025a05bac2d67d0cc56e56120f926effb75
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-01-17 18:39:17 +0530

    Socket connection & sending messages in UI

--- src/component/Chat.jsx
+++ src/component/Chat.jsx
@@ -1,34 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { createSocketConnection } from "../utils/socket.js";
+import { useSelector } from "react-redux";
 
 const Chat = () => {
   const { targetUserId } = useParams();
-  console.log(targetUserId);
+  const user = useSelector((store) => store.user);
+  const userId = user?._id;
+  const [newMessage, SetNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const socket = createSocketConnection();
+
+    socket.emit("joinChat", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+    });
+
+    socket.on("messageReceived", ({ firstName, text }) => {
+      setMessages((messages) => [...messages, { firstName, text }]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [userId, targetUserId]);
+
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+      text: newMessage,
+    });
+
+    SetNewMessage("");
+  };
 
   return (
-    <div className="w-1/2 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
+    <div className="w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
       <h1 className="p-5 bg-neutral border-b border-gray-600">Chat</h1>
-      <div className="flex-1 overflow-scroll p-5">
-        <div className="chat chat-start">
-          <div className="chat-image avatar">
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS chat bubble component"
-                src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
-              />
+      <div className="flex-1 overflow-y-scroll p-5">
+        {messages.map((message, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                "chat " +
+                (user.firstName === message.firstName
+                  ? "chat-end"
+                  : "chat-start")
+              }
+            >
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header">
+                {message.firstName}
+                <time className="text-xs opacity-50">12:45</time>
+              </div>
+              <div className="chat-bubble">{message.text}</div>
+              <div className="chat-footer opacity-50">Delivered</div>
             </div>
-          </div>
-          <div className="chat-header">
-            Obi-Wan Kenobi
-            <time className="text-xs opacity-50">12:45</time>
-          </div>
-          <div className="chat-bubble">You were the Chosen One!</div>
-          <div className="chat-footer opacity-50">Delivered</div>
-        </div>
+          );
+        })}
       </div>
       <div className="p-5 bg-neutral border-t border-gray-600 flex items-end gap-2">
-        <input className="flex-1 border border-gray-500 text-white rounded p-2 opacity-30" />
-        <button className="btn btn-primary">Send</button>
+        <input
+          value={newMessage}
+          onChange={(e) => SetNewMessage(e.target.value)}
+          className="flex-1 border border-gray-500 text-white rounded p-2"
+        />
+        <button onClick={sendMessage} className="btn btn-primary">
+          Send
+        </button>
       </div>
     </div>
   );
